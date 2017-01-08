244503cb0976565b02945d9572745c4c1931aa56
xuecanzhao <461900137@qq.com>
2017-01-08 22:00:28 +0800

    change main

--- TEST1/main.cpp
+++ TEST1/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int calculateMax(vector<int> prices) {
 	int firstBuy = INT_MIN, firstSell = 0;
 	int secondBuy = INT_MIN, secondSell = 0;
-
+	//test
 	for (int curPrice : prices) {
 		if (firstBuy < -curPrice) firstBuy = -curPrice; // the max profit after you buy first stock
 		if (firstSell < firstBuy + curPrice) firstSell = firstBuy + curPrice; // the max profit after you sell it
